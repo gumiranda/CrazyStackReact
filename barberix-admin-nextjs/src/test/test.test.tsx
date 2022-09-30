ec23b5ed630854e21ad2e5edba59cf9079497cf1
Gustavo <gustavo.aguiar@redspark.io>
2022-09-30 16:45:11 -0300

    Aula 2 - Correção pro build

--- barberix-admin-nextjs/src/test/test.test.tsx
+++ barberix-admin-nextjs/src/test/test.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import Home from "./index";
+import Home from "../pages/index";
 
 describe("first test", () => {
   it("should render div", () => {
