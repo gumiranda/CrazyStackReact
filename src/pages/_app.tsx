e65ba944e9157de1908c39e7f983418f6c786148
Gustavo <gustavo.aguiar@redspark.io>
2022-10-06 15:13:07 -0300

    Aula 3 - Instalando Chakra UI no projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export const theme = extendTheme({ colors });
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-
-export default MyApp;
+export default App;
