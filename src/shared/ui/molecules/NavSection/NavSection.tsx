c9617694ce9b43aba88c53b6566d18f900a0c027
gustavo.aguiar <gustavo.aguiar@redspark.io>
2022-12-28 10:54:57 -0300

    Aula 32 - Ajuste sidebar

--- src/shared/ui/molecules/NavSection/NavSection.tsx
+++ src/shared/ui/molecules/NavSection/NavSection.tsx
@@ -1,19 +1,27 @@
 import { Box, Text } from "shared/ui/atoms";
 import { ReactNode } from "react";
-import { HStack } from "@chakra-ui/react";
+import { HStack, Stack, useBreakpointValue } from "@chakra-ui/react";
 interface NavSectionProps {
   title: string;
   children: ReactNode;
 }
 export const NavSection = ({ title, children, ...rest }: NavSectionProps) => {
+  const isDesktopVersion = useBreakpointValue({ base: false, lg: true });
+
   return (
     <Box {...rest} datatestid="NavSectionTestId">
       <Text fontWeight={"bold"} color="purple.400" fontSize="small">
         {title}
       </Text>
-      <HStack spacing="4" mt="8" align="stretch">
-        {children}
-      </HStack>
+      {isDesktopVersion ? (
+        <HStack spacing="4" mt="8" align="stretch">
+          {children}
+        </HStack>
+      ) : (
+        <Stack spacing="4" mt="8" align="stretch">
+          {children}
+        </Stack>
+      )}
     </Box>
   );
 };
