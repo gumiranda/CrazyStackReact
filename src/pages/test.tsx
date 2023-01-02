4dc6b812a04a2297b1b3b1486a4e4429ab7577aa
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-01-02 12:00:29 -0300

    update branch aula1

--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Home from "./index";
-
-describe("first test", () => {
-  it("should render div", () => {
-    const { container } = render(<Home />);
-    expect(container.querySelector("div")).toBeInTheDocument();
-  });
-});
--- src/test/test.test.tsx
+++ src/test/test.test.tsx
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import Home from "../pages/index";
+import React from "react";
 
 describe("first test", () => {
   it("should render div", () => {
