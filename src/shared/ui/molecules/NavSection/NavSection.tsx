b87007733541d633d924c507f8ec08d52ce36aaf
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-01-02 16:46:35 -0300

    format

--- src/shared/ui/molecules/NavSection/NavSection.tsx
+++ src/shared/ui/molecules/NavSection/NavSection.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+//@ts-ignore
 import { Box, Text } from "shared/ui/atoms";
 import { ReactNode } from "react";
 import { HStack, Stack, useBreakpointValue } from "@chakra-ui/react";
@@ -6,21 +8,27 @@ interface NavSectionProps {
   children: ReactNode;
 }
 export const NavSection = ({ title, children, ...rest }: NavSectionProps) => {
-  const isDesktopVersion = useBreakpointValue({ base: false, lg: true });
   return (
     <Box {...rest} datatestid="NavSectionTestId">
       <Text fontWeight={"bold"} color="purple.400" fontSize="small">
         {title}
       </Text>
-      {isDesktopVersion ? (
-        <HStack spacing="4" mt="8" align="stretch">
-          {children}
-        </HStack>
-      ) : (
-        <Stack spacing="4" mt="8" align="stretch">
-          {children}
-        </Stack>
-      )}
+      <CustomStack>{children}</CustomStack>
     </Box>
+  );
+};
+const CustomStack = ({ children }: any) => {
+  const isDesktopVersion = useBreakpointValue({ base: false, lg: true });
+  if (isDesktopVersion) {
+    return (
+      <HStack spacing="4" mt="8" align="stretch">
+        {children}
+      </HStack>
+    );
+  }
+  return (
+    <Stack spacing="4" mt="8" align="stretch">
+      {children}
+    </Stack>
   );
 };
