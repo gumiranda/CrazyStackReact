0fd137ee1e760a4d8b50a37e864eca8a0c3fbdad
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-01-04 09:40:06 -0300

    aula 30 - 2

---
+++ src/entidades/appointment/timeAvailable.model.ts
@@ -0,0 +1,63 @@
+export type TimeAvailableProps = {
+  ownerId: string;
+  serviceId: string;
+  timeAvailable: any[];
+  timeAvailableProfessional: any[];
+  value?: boolean;
+};
+
+class TimeAvailable {
+  protected props: TimeAvailableProps;
+  constructor(props: TimeAvailableProps) {
+    this.props = props;
+  }
+  public static build(props: TimeAvailableProps) {
+    return new TimeAvailable(props);
+  }
+  get ownerId(): string {
+    return this.props.ownerId;
+  }
+  get serviceId(): string {
+    return this.props.serviceId;
+  }
+  get timeAvailable(): any[] {
+    return this.props.timeAvailable;
+  }
+  get timeAvailableProfessional(): any[] {
+    return this.props.timeAvailable;
+  }
+  format(): TimeAvailableProps {
+    return {
+      ownerId: this.props.ownerId,
+      serviceId: this.props.serviceId,
+      value: false,
+      timeAvailable: this.props.timeAvailable.map(({ time }: any) => ({
+        label: new Date(time)
+          .toLocaleDateString("pt-BR", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })
+          ?.split?.(" ")?.[1],
+        value: new Date(time).toISOString(),
+      })),
+      timeAvailableProfessional: this.props.timeAvailableProfessional.map(
+        ({ initDate, endDate }: any) => ({
+          initDate: new Date(initDate)
+            .toLocaleDateString("pt-BR", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })
+            ?.split?.(" ")?.[1],
+          endDate: new Date(endDate)
+            .toLocaleDateString("pt-BR", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })
+            ?.split?.(" ")?.[1],
+        })
+      ),
+    };
+  }
+}
+export const timeAvailableModel = (props: TimeAvailableProps) =>
+  TimeAvailable.build(props);
--- src/entidades/request/request.model.ts
+++ src/entidades/request/request.model.ts
@@ -1,9 +1,24 @@
+import { startOfDay } from "date-fns";
+
 export type RequestProps = {
   _id: string;
-  name: string;
+  status: number;
+  message: string;
   createdAt: string;
   value?: boolean;
   active?: boolean;
+  clientId: string;
+  professionalId: string;
+  serviceId: string;
+  ownerId: string;
+  createdForId: string;
+  initDate: string;
+  endDate: string;
+  date: string;
+  haveRecurrence: boolean;
+  haveRide: boolean;
+  haveFidelity: boolean;
+  haveDelivery: boolean;
 };
 
 class Request {
@@ -17,8 +32,8 @@ class Request {
   get _id(): string {
     return this.props._id;
   }
-  get name(): string {
-    return this.props.name;
+  get message(): string {
+    return this.props.message;
   }
   get createdAt(): string {
     return this.props.createdAt;
@@ -26,13 +41,54 @@ class Request {
   get active(): boolean | undefined {
     return this.props.active;
   }
+  get status(): number {
+    return this.props.status;
+  }
+  get ownerId(): string {
+    return this.props.ownerId;
+  }
+  get clientId(): string {
+    return this.props.clientId;
+  }
+  get professionalId(): string {
+    return this.props.professionalId;
+  }
+  get serviceId(): string {
+    return this.props.serviceId;
+  }
+  get createdForId(): string {
+    return this.props.createdForId;
+  }
+  get initDate(): string {
+    return this.props.initDate;
+  }
+  get endDate(): string {
+    return this.props.endDate;
+  }
+  get date(): string {
+    return this.props.date;
+  }
+  get haveRecurrence(): boolean {
+    return this.props.haveRecurrence;
+  }
+  get haveRide(): boolean {
+    return this.props.haveRide;
+  }
+  get haveFidelity(): boolean {
+    return this.props.haveFidelity;
+  }
+  get haveDelivery(): boolean {
+    return this.props.haveDelivery;
+  }
+
   format(): RequestProps {
     return {
       ...this.props,
       _id: this.props._id,
-      name: this.props.name,
+      message: this.props.message,
       active: this.props.active,
       value: false,
+      date: startOfDay(new Date(this.props.initDate)).toISOString(),
       createdAt: new Date(this.props.createdAt).toLocaleDateString("pt-BR", {
         day: "2-digit",
         month: "2-digit",
