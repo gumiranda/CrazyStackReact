cc8d4bb471deb2dab1eec82f6fd7c3fdaeef1785
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-01-19 12:38:30 -0300

    Aula 29 - Select Múltiplo

--- src/entidades/owner/index.ts
+++ src/entidades/owner/index.ts
@@ -2,3 +2,4 @@ export { useGetOwners } from "./owner.lib";
 export { getOwners } from "./owner.api";
 export type { GetOwnersResponse } from "./owner.api";
 export type { OwnerProps } from "./owner.model";
+export * from "./hourWork";
--- src/shared/ui/atoms/index.ts
+++ src/shared/ui/atoms/index.ts
@@ -1,6 +1,7 @@
 export * from "./Head";
 export * from "./Flex";
 // IMPORT MODULE FILES
+export * from "./ControlledSelect";
 export * from "./HStack";
 export * from "./Select";
 export * from "./Card";
