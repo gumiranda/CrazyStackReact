43de34edfa5572ae8b2ac2b3d330b5e8da9f1c5a
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-01-25 11:24:34 -0300

    Aula 30 - Update client and user list hooks

--- src/features/client/clientList.hook.ts
+++ src/features/client/clientList.hook.ts
@@ -1,36 +1,50 @@
-import { ClientProps } from "entidades/client";
-import { ClientProps, GetClientsResponse, getClients } from "entidades/client";
+import { GetClientsResponse, getClients, ClientProps } from "entidades/client";
 import { useState, useEffect } from "react";
 export type ClientFormProps = {
-  clientList: GetClientsResponse;
+  clientList?: GetClientsResponse | null;
   currentClient?: ClientProps;
+  ownerSelected?: string | null;
 };
-
-export const useClientsSelect = ({ clientList, currentClient }: ClientFormProps) => {
+export const useClientsSelect = ({
+  clientList = null,
+  currentClient,
+  ownerSelected = null,
+}: ClientFormProps) => {
   const [page, setPage] = useState(1);
-  const [clients, setClients] = useState<ClientProps[]>(clientList?.clients);
-  const [clientSelected, setClientSelected] = useState<string | undefined>(
-    currentClient?.clientId ?? clientList?.clients?.[0]?._id ?? ""
+  const [clients, setClients] = useState(clientList?.clients ?? []);
+  const [clientSelected, setClientSelected] = useState<string>(
+    //currentClient?._id ??
+    clientList?.clients?.[0]?._id ?? ""
   );
-  const handleChangeClientSelected = (event: any): void => {
+  const handleChangeClientSelected = (event: any) => {
     event.preventDefault();
     setClientSelected(event.target.value);
   };
   const fetchClientsPaginated = async () => {
-    if (clientList?.totalCount > clients?.length && page > 1) {
-      const data = await getClients(page, null);
-      if (data?.totalCount > clients?.length) {
-        setClients((prev) => [...prev, ...(data?.clients ?? [])]);
+    if (clientList && clientList?.totalCount > clients?.length && page > 1) {
+      const params = {};
+      if (ownerSelected) {
+        Object.assign(params, { createdById: ownerSelected });
       }
-      setClientSelected(
-        data?.clients?.[0]?._id ?? clients?.[0]?._id ?? currentClient?.clientId ?? ""
-      );
+      const data = await getClients(page, null, params);
+      if (data?.totalCount > clients?.length) {
+        setClients((prev) => [...prev, ...(data.clients ?? [])]);
+      }
+      setClientSelected(data?.clients?.[0]?._id ?? clients?.[0]?._id ?? "");
+    } else if (!clientList && ownerSelected) {
+      const data = await getClients(page, null, {
+        createdById: ownerSelected,
+      });
+      if (data?.totalCount > clients?.length) {
+        setClients((prev) => [...prev, ...(data.clients ?? [])]);
+      }
+      setClientSelected(data?.clients?.[0]?._id ?? clients?.[0]?._id ?? "");
     } else {
-      setClientSelected(clients?.[0]?._id ?? currentClient?.clientId ?? "");
+      setClientSelected(clients?.[0]?._id ?? "");
     }
   };
   useEffect(() => {
-    setClients(clientList?.clients);
+    setClients(clientList?.clients ?? []);
   }, [clientList?.clients]);
   useEffect(() => {
     if (clientSelected === "loadMore") {
--- src/features/user/userList.hook.ts
+++ src/features/user/userList.hook.ts
@@ -1,36 +1,54 @@
-import { UserProps } from "entidades/user";
-import { UserProps, GetUsersResponse, getUsers } from "entidades/user";
+import { GetUsersResponse, getUsers, UserProps } from "entidades/user";
 import { useState, useEffect } from "react";
 export type UserFormProps = {
-  userList: GetUsersResponse;
+  userList?: GetUsersResponse | null;
   currentUser?: UserProps;
+  ownerSelected?: string | null;
+  role?: string;
 };
-
-export const useUsersSelect = ({ userList, currentUser }: UserFormProps) => {
+export const useUsersSelect = ({
+  userList = null,
+  currentUser,
+  ownerSelected = null,
+  role = "professional",
+}: UserFormProps) => {
+  console.log(userList);
   const [page, setPage] = useState(1);
-  const [users, setUsers] = useState<UserProps[]>(userList?.users);
-  const [userSelected, setUserSelected] = useState<string | undefined>(
-    currentUser?.userId ?? userList?.users?.[0]?._id ?? ""
+  const [users, setUsers] = useState(userList?.users ?? []);
+  const [userSelected, setUserSelected] = useState<string>(
+    //currentUser?.usersId ??
+    userList?.users?.[0]?._id ?? ""
   );
-  const handleChangeUserSelected = (event: any): void => {
+  const handleChangeUserSelected = (event: any) => {
     event.preventDefault();
     setUserSelected(event.target.value);
   };
   const fetchUsersPaginated = async () => {
-    if (userList?.totalCount > users?.length && page > 1) {
-      const data = await getUsers(page, null);
-      if (data?.totalCount > users?.length) {
-        setUsers((prev) => [...prev, ...(data?.users ?? [])]);
+    if (userList && userList?.totalCount > users?.length && page > 1) {
+      const params = { role };
+      if (ownerSelected) {
+        Object.assign(params, { ownerId: ownerSelected });
       }
-      setUserSelected(
-        data?.users?.[0]?._id ?? users?.[0]?._id ?? currentUser?.userId ?? ""
-      );
+      const data = await getUsers(page, null, params);
+      if (data?.totalCount > users?.length) {
+        setUsers((prev) => [...prev, ...(data.users ?? [])]);
+      }
+      setUserSelected(data?.users?.[0]?._id ?? users?.[0]?._id ?? "");
+    } else if (!userList && ownerSelected) {
+      const data = await getUsers(page, null, {
+        ownerId: ownerSelected,
+        role,
+      });
+      if (data?.totalCount > users?.length) {
+        setUsers((prev) => [...prev, ...(data.users ?? [])]);
+      }
+      setUserSelected(data?.users?.[0]?._id ?? users?.[0]?._id ?? "");
     } else {
-      setUserSelected(users?.[0]?._id ?? currentUser?.userId ?? "");
+      setUserSelected(users?.[0]?._id ?? "");
     }
   };
   useEffect(() => {
-    setUsers(userList?.users);
+    setUsers(userList?.users ?? []);
   }, [userList?.users]);
   useEffect(() => {
     if (userSelected === "loadMore") {
