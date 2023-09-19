a428666bb22a04d703972df42b4da9b159c8f128
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-09-19 17:12:45 -0300

    preview da corrida

--- src/features/mapRoute/create/CreateMapRouteForm.tsx
+++ src/features/mapRoute/create/CreateMapRouteForm.tsx
@@ -1,8 +1,10 @@
-import { MapRouteProps } from "entidades/mapRoute";
 import { useCreateMapRoute } from "./createMapRoute.hook";
 import { BoxCreateItem, FormControl, Checkbox, GridForm } from "shared/ui";
+import { useRef } from "react";
+import { Grid, Text, List, ListItem, Card, CardBody, Button } from "@chakra-ui/react";
 
 export const CreateMapRouteForm = () => {
+  const mapContainerRef = useRef<HTMLDivElement>(null);
   const {
     formState,
     register,
@@ -12,48 +14,96 @@ export const CreateMapRouteForm = () => {
     setActive,
     originListPlaces,
     destinationListPlaces,
-  } = useCreateMapRoute();
+    directionsData,
+    fetchDirections,
+    originText,
+    destinationText,
+  } = useCreateMapRoute({ mapContainerRef });
   return (
-    <BoxCreateItem
-      onSubmit={handleSubmit(handleCreateMapRoute)}
-      title={"Criar rotas"}
-      isLoadingSaveButton={formState.isSubmitting}
-      cancelRoute={"/mapRoutes/1"}
-    >
-      <GridForm>
-        <FormControl
-          label="Nome da rota"
-          error={formState.errors.name}
-          {...register("name")}
-        />
-        <FormControl
-          label="Origem"
-          error={formState.errors.originText}
-          autoCompleteProps={{
-            list: originListPlaces,
-            placeholder: "Digite para pesquisar a origem",
-          }}
-          {...register("originText")}
-        />
-        <FormControl
-          label="Destino"
-          error={formState.errors.destinationText}
-          autoCompleteProps={{
-            list: destinationListPlaces,
-            placeholder: "Digite para pesquisar a destino",
-          }}
-          {...register("destinationText")}
-        />
-        <Checkbox
-          label="Ativo"
+    <>
+      <BoxCreateItem
+        onSubmit={handleSubmit(handleCreateMapRoute)}
+        title={"Criar rotas"}
+        isLoadingSaveButton={formState.isSubmitting}
+        cancelRoute={"/mapRoutes/1"}
+      >
+        <GridForm>
+          <FormControl
+            label="Nome da rota"
+            error={formState.errors.name}
+            {...register("name")}
+          />
+          <FormControl
+            label="Origem"
+            error={formState.errors.originText}
+            autoCompleteProps={{
+              list: originListPlaces,
+              placeholder: "Digite para pesquisar a origem",
+            }}
+            {...register("originText")}
+          />
+          <FormControl
+            label="Destino"
+            error={formState.errors.destinationText}
+            autoCompleteProps={{
+              list: destinationListPlaces,
+              placeholder: "Digite para pesquisar a destino",
+            }}
+            {...register("destinationText")}
+          />
+          <Checkbox
+            label="Ativo"
+            colorScheme="green"
+            isChecked={active}
+            onChange={(e) => {
+              e.preventDefault();
+              setActive(e.target.checked);
+            }}
+          />
+        </GridForm>
+
+        {directionsData && (
+          <Card mt={1}>
+            <CardBody>
+              <List>
+                <ListItem>
+                  <Text>Origem</Text>
+                  <Text>{directionsData?.routes[0]!.legs[0]!.start_address}</Text>
+                </ListItem>
+                <ListItem>
+                  <Text>Destino</Text>
+                  <Text>{directionsData?.routes[0]!.legs[0]!.end_address}</Text>
+                </ListItem>
+                <ListItem>
+                  <Text>Distância</Text>
+                  <Text>{directionsData?.routes[0]!.legs[0]!.distance.text}</Text>
+                </ListItem>
+                <ListItem>
+                  <Text>Duração</Text>
+                  <Text>{directionsData?.routes[0]!.legs[0]!.duration.text}</Text>
+                </ListItem>
+              </List>
+            </CardBody>
+          </Card>
+        )}
+      </BoxCreateItem>
+      {originText?.length > 0 && destinationText?.length > 0 && (
+        <Button
+          bgColor="green.500"
           colorScheme="green"
-          isChecked={active}
-          onChange={(e) => {
+          variant="contained"
+          onClick={(e) => {
             e.preventDefault();
-            setActive(e.target.checked);
+            fetchDirections();
           }}
-        />
-      </GridForm>
-    </BoxCreateItem>
+          mt={1}
+          mb={2}
+        >
+          Ver rotas
+        </Button>
+      )}
+
+      <Grid id="map" p={40} ref={mapContainerRef}></Grid>
+    </>
   );
 };
--- src/features/mapRoute/create/createMapRoute.hook.ts
+++ src/features/mapRoute/create/createMapRoute.hook.ts
@@ -7,10 +7,16 @@ import {
 import { useRouter } from "next/router";
 import { api } from "shared/api";
 import { useMutation } from "@tanstack/react-query";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { parseCookies } from "nookies";
+import type { DirectionsResponseData } from "@googlemaps/google-maps-services-js";
+import { useLoadMap } from "../load-map";
 
-export const useCreateMapRoute = () => {
+export const useCreateMapRoute = ({ mapContainerRef }: any) => {
+  const map = useLoadMap(mapContainerRef);
+  const [directionsData, setDirectionsData] = useState<
+    DirectionsResponseData & { request: any }
+  >();
   const { showModal } = useUi();
   const router = useRouter();
   const [active, setActive] = useState(false);
@@ -121,6 +127,47 @@ export const useCreateMapRoute = () => {
       setTimeoutId(id);
     }
   }, [destinationText]);
+  const fetchDirections = useCallback(async () => {
+    const cookies = parseCookies();
+    const source = (document.getElementById("originText") as HTMLInputElement).value;
+    const destination = (document.getElementById("destinationText") as HTMLInputElement)
+      .value;
+    const currentOrigin: any = originListPlaces?.find?.(
+      (item: any) => item?.label === source
+    );
+    const currentDestination: any = destinationListPlaces?.find?.(
+      (item: any) => item?.label === destination
+    );
+
+    if (!currentOrigin) {
+      return;
+    }
+
+    if (!currentDestination) {
+      return;
+    }
+    const directionsResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_NEXT_API_URL}/directions?originId=${currentOrigin?.value}&destinationId=${currentDestination?.value}`,
+      { headers: { authorization: `Bearer ${cookies["belezixadmin.token"]}` } }
+    );
+    const directionsData: DirectionsResponseData & { request: any } =
+      await directionsResponse.json();
+    setDirectionsData(directionsData);
+    map?.removeAllRoutes();
+    await map?.addRouteWithIcons({
+      routeId: "1",
+      startMarkerOptions: {
+        position: directionsData?.routes?.[0]?.legs?.[0]?.start_location,
+      },
+      endMarkerOptions: {
+        position: directionsData?.routes?.[0]?.legs?.[0]?.end_location,
+      },
+      carMarkerOptions: {
+        position: directionsData?.routes?.[0]?.legs?.[0]?.start_location,
+      },
+    });
+  }, [originText, destinationText, originListPlaces, destinationListPlaces]); // Specify dependencies here
+
   return {
     formState,
     register,
@@ -130,5 +177,9 @@ export const useCreateMapRoute = () => {
     setActive,
     originListPlaces,
     destinationListPlaces,
+    fetchDirections,
+    directionsData,
+    destinationText,
+    originText,
   };
 };
