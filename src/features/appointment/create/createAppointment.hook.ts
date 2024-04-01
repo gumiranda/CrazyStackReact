441ca70366d656a07cce9d38fca87efa6e613643
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-04-01 19:18:02 -0300

    Atualizar importações de roteador em createAppointment.hook.ts e editAppointment.hook.ts

--- src/features/appointment/create/createAppointment.hook.ts
+++ src/features/appointment/create/createAppointment.hook.ts
@@ -4,7 +4,7 @@ import {
   SubmitCreateAppointmentHandler,
   useCreateAppointmentLib,
 } from "./createAppointment.lib";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { api } from "@/shared/api";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
@@ -12,38 +12,43 @@ export const useCreateAppointment = () => {
   const { showModal } = useUi();
   const router = useRouter();
   const [active, setActive] = useState(false);
-  const createAppointment = useMutation(
-    async (appointment: CreateAppointmentFormData) => {
-      try {
-        const { data } = await api.post("/appointment/add", {
-          ...appointment,
-        });
-        if (!data) {
-          showModal({
-            content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
-            title: "Erro no servidor",
-            type: "error",
-          });
-          return;
-        }
-        showModal({
-          content:
-            "Agendamento criada com sucesso, você será redirecionado para a lista de agendamentos",
-          title: "Sucesso",
-          type: "success",
-        });
-        router.push("/appointments/1");
-        return data;
-      } catch (error) {
+  const createFunction = async (appointment: CreateAppointmentFormData) => {
+    try {
+      const { data } = await api.post("/appointment/add", {
+        ...appointment,
+      });
+      if (!data) {
         showModal({
           content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
           title: "Erro no servidor",
           type: "error",
         });
+        return;
       }
-    },
-    {}
-  );
+      showModal({
+        content:
+          "Agendamento criada com sucesso, você será redirecionado para a lista de agendamentos",
+        title: "Sucesso",
+        type: "success",
+      });
+      router.push("/appointments/1");
+      return data;
+    } catch (error) {
+      showModal({
+        content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
+        title: "Erro no servidor",
+        type: "error",
+      });
+    }
+  };
+  const createAppointment = useMutation(
+    createFunction as any,
+    {
+      onError: (error: any) => {
+        console.error(error);
+      },
+    } as any
+  ) as any;
   const { register, handleSubmit, formState } = useCreateAppointmentLib();
   const handleCreateAppointment: SubmitCreateAppointmentHandler = async (
     values: CreateAppointmentFormData
