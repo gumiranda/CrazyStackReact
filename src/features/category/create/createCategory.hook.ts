5dfa7172a64a0622508e6483d9945bd55a42e586
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-04-01 19:19:01 -0300

    Atualizações no formulário de login e na criação de categorias

--- src/features/category/create/createCategory.hook.ts
+++ src/features/category/create/createCategory.hook.ts
@@ -1,10 +1,11 @@
+"use client";
 import { useUi } from "@/shared/libs";
 import {
   CreateCategoryFormData,
   SubmitCreateCategoryHandler,
   useCreateCategoryLib,
 } from "./createCategory.lib";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { api } from "@/shared/api";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
@@ -12,40 +13,49 @@ export const useCreateCategory = () => {
   const { showModal } = useUi();
   const router = useRouter();
   const [active, setActive] = useState(false);
-  const createCategory = useMutation(async (category: CreateCategoryFormData) => {
-    try {
-      const { data } = await api.post("/category/add", {
-        ...category,
-      });
-      if (!data) {
+  const createCategory = useMutation({
+    mutationFn: async (category: CreateCategoryFormData) => {
+      try {
+        const { data } = await api.post("/category/add", {
+          ...category,
+        });
+        if (!data) {
+          showModal({
+            content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
+            title: "Erro no servidor",
+            type: "error",
+          });
+          return;
+        }
+        showModal({
+          content:
+            "Categoria criada com sucesso, você será redirecionado para a lista de categorias",
+          title: "Sucesso",
+          type: "success",
+        });
+        router.push("/categorys/1");
+        return data;
+      } catch (error) {
         showModal({
           content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
           title: "Erro no servidor",
           type: "error",
         });
-        return;
       }
-      showModal({
-        content:
-          "Categoria criada com sucesso, você será redirecionado para a lista de categorias",
-        title: "Sucesso",
-        type: "success",
-      });
-      router.push("/categorys/1");
-      return data;
-    } catch (error) {
-      showModal({
-        content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
-        title: "Erro no servidor",
-        type: "error",
-      });
-    }
-  }, {});
+    },
+  });
   const { register, handleSubmit, formState } = useCreateCategoryLib();
   const handleCreateCategory: SubmitCreateCategoryHandler = async (
     values: CreateCategoryFormData
   ) => {
     await createCategory.mutateAsync({ ...values, active });
   };
-  return { formState, register, handleSubmit, handleCreateCategory, active, setActive };
+  return {
+    formState,
+    register,
+    handleSubmit,
+    handleCreateCategory,
+    active,
+    setActive,
+  };
 };
