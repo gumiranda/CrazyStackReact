da8ab35fb81d744e256fcfe6b884352063bcae21
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-04-01 19:22:06 -0300

    Atualiza o uso do pacote next/navigation no arquivo editMapRoute.hook.ts

--- src/features/mapRoute/edit/editMapRoute.hook.ts
+++ src/features/mapRoute/edit/editMapRoute.hook.ts
@@ -5,7 +5,7 @@ import {
   SubmitEditMapRouteHandler,
   useEditMapRouteLib,
 } from "./editMapRoute.lib";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { api } from "@/shared/api";
 import { useMutation } from "@tanstack/react-query";
 import { useHandleLocation } from "../hooks";
@@ -14,36 +14,38 @@ export const useEditMapRoute = (props: EditMapRouteFormProps) => {
   const { showModal } = useUi();
   const { mapRoute: currentMapRoute, mapContainerRef } = props;
   const router = useRouter();
-  const editMapRoute = useMutation(async (mapRoute: EditMapRouteFormData) => {
-    try {
-      const { data } = await api.patch(`/mapRoute/update?_id=${currentMapRoute._id}`, {
-        ...mapRoute,
-        updatedAt: new Date(),
-      });
-      if (!data) {
+  const editMapRoute = useMutation({
+    mutationFn: async (mapRoute: EditMapRouteFormData) => {
+      try {
+        const { data } = await api.patch(`/mapRoute/update?_id=${currentMapRoute._id}`, {
+          ...mapRoute,
+          updatedAt: new Date(),
+        });
+        if (!data) {
+          showModal({
+            content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
+            title: "Erro no servidor",
+            type: "error",
+          });
+          return;
+        }
+        showModal({
+          content:
+            "Rota editada com sucesso," + "você será redirecionado para a lista de rotas",
+          title: "Sucesso",
+          type: "success",
+        });
+        router.push("/mapRoutes/1");
+        return data;
+      } catch (error) {
         showModal({
           content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
           title: "Erro no servidor",
           type: "error",
         });
-        return;
       }
-      showModal({
-        content:
-          "Rota editada com sucesso," + "você será redirecionado para a lista de rotas",
-        title: "Sucesso",
-        type: "success",
-      });
-      router.push("/mapRoutes/1");
-      return data;
-    } catch (error) {
-      showModal({
-        content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
-        title: "Erro no servidor",
-        type: "error",
-      });
-    }
-  }, {});
+    },
+  });
   const { register, handleSubmit, formState, watch } = useEditMapRouteLib(props);
 
   const originText = watch("originText");
