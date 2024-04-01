19ba83eb2a6a9465f4d291767d9d3e9f89e3a31a
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-04-01 19:19:40 -0300

    Adiciona importação do módulo "client" e corrige importação do módulo "next/navigation"

--- src/features/category/edit/editCategory.hook.ts
+++ src/features/category/edit/editCategory.hook.ts
@@ -1,3 +1,4 @@
+"use client";
 import { useUi } from "@/shared/libs";
 import { EditCategoryFormProps } from "./EditCategoryForm";
 import {
@@ -5,43 +6,45 @@ import {
   SubmitEditCategoryHandler,
   useEditCategoryLib,
 } from "./editCategory.lib";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { api } from "@/shared/api";
 import { useMutation } from "@tanstack/react-query";
 export const useEditCategory = (props: EditCategoryFormProps) => {
   const { showModal } = useUi();
   const { category: currentCategory } = props;
   const router = useRouter();
-  const editCategory = useMutation(async (category: EditCategoryFormData) => {
-    try {
-      const { data } = await api.patch(`/category/update?_id=${currentCategory._id}`, {
-        ...category,
-        updatedAt: new Date(),
-      });
-      if (!data) {
+  const editCategory = useMutation({
+    mutationFn: async (category: EditCategoryFormData) => {
+      try {
+        const { data } = await api.patch(`/category/update?_id=${currentCategory._id}`, {
+          ...category,
+          updatedAt: new Date(),
+        });
+        if (!data) {
+          showModal({
+            content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
+            title: "Erro no servidor",
+            type: "error",
+          });
+          return;
+        }
+        showModal({
+          content:
+            "Categoria editada com sucesso, você será redirecionado para a lista de categorias",
+          title: "Sucesso",
+          type: "success",
+        });
+        router.push("/categorys/1");
+        return data;
+      } catch (error) {
         showModal({
           content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
           title: "Erro no servidor",
           type: "error",
         });
-        return;
       }
-      showModal({
-        content:
-          "Categoria editada com sucesso, você será redirecionado para a lista de categorias",
-        title: "Sucesso",
-        type: "success",
-      });
-      router.push("/categorys/1");
-      return data;
-    } catch (error) {
-      showModal({
-        content: "Ocorreu um erro inesperado no servidor, tente novamente mais tarde",
-        title: "Erro no servidor",
-        type: "error",
-      });
-    }
-  }, {});
+    },
+  });
   const { register, handleSubmit, formState } = useEditCategoryLib(props);
   const handleEditCategory: SubmitEditCategoryHandler = async (
     values: EditCategoryFormData
