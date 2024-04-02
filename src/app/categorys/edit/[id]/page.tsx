5f7855301b7b68cb3557b897183b5143be409af4
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-04-02 18:03:58 -0300

    Adiciona página de edição de categorias e atualiza importações

---
+++ src/app/categorys/edit/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { getCategoryById } from "@/entidades/category/category.api";
+import { getCookies, parseCookies } from "@/shared/libs/utils";
+export const revalidate = 3000;
+
+import { config } from "@/application/config";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { CategoryEditPage } from "@/screens/category/edit";
+
+export const metadata: Metadata = {
+  title: `${config.systemName} | Editar Categoria`,
+  description: `Página de edição de categorias do ${config.systemName}. Aqui você pode editar uma categoria.`,
+};
+
+async function getData(id) {
+  const allCookies = getCookies();
+  const parsedCookies = parseCookies(allCookies);
+  if (!parsedCookies?.["belezixadmin.token"]) {
+    return null;
+  }
+  const res = await getCategoryById(id, parsedCookies);
+  if (!res) {
+    return null;
+  }
+  return res;
+}
+export default async function Page({ params: { id } }: { params: { id: string } }) {
+  const data = await getData(id);
+  if (!data) {
+    redirect("/login");
+  }
+  return <CategoryEditPage data={data} id={id} />;
+}
--- src/screens/category/edit/CategoryEditPage.tsx
+++ src/screens/category/edit/CategoryEditPage.tsx
@@ -1,4 +1,5 @@
-import { Box, Head } from "@/shared/ui";
+"use client";
+import { Box } from "@/shared/ui";
 import { EditCategoryForm } from "@/features/category/edit";
 import { CategoryProps } from "@/entidades/category";
 type CategoryEditProps = {
@@ -9,10 +10,6 @@ export const CategoryEditPage = ({ data, id }: CategoryEditProps) => {
   const props = { category: data };
   return (
     <>
-      <Head
-        title={"Belezix Admin | Categorias"}
-        description="Página de edição de categorias do painel de Admin Belezix"
-      />
       <Box flex="1" borderRadius={8} bg="secondary.500" p="8">
         <EditCategoryForm {...props} />
       </Box>
