0a6823facd6b78dd3a9876c1933077333f33b256
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-04-02 17:40:53 -0300

    Adiciona arquivos de criação de categoria e tratamento de erro

--- src/screens/category/create/CategoryCreatePage.tsx
+++ src/screens/category/create/CategoryCreatePage.tsx
@@ -1,14 +1,10 @@
-import { Box, Head } from "@/shared/ui";
-import { CategoryProps } from "@/entidades/category";
+"use client";
+import { Box } from "@/shared/ui";
 import { CreateCategoryForm } from "@/features/category/create";
 
 export const CategoryCreatePage = () => {
   return (
     <>
-      <Head
-        title={"Belezix Admin | Categorias"}
-        description="Página de criação de categorias do painel de Admin Belezix"
-      />
       <Box flex="1" borderRadius={8} bg="purple.800" p="8">
         <CreateCategoryForm />
       </Box>
