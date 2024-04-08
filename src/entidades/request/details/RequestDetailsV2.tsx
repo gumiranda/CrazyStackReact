7ca7a625a2083dd0b1505ac3a726b10ce48fb899
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-04-08 10:00:47 -0300

    Adiciona campos de visualização e formatação de preço na tela de detalhes do pedido

--- src/entidades/request/details/RequestDetailsV2.tsx
+++ src/entidades/request/details/RequestDetailsV2.tsx
@@ -5,5 +5,53 @@ import { useRequestDetailsOwner } from "./useRequestDetailsOwner";
 import { useRouter } from "next/navigation";
 
 export const RequestDetailsV2 = ({ props: { request, onClose } }) => {
-  return <></>;
+  const router = useRouter();
+  const { serviceId, clientId } = request;
+  const { service, client, deleteSelectedAction }: any = useRequestDetailsOwner({
+    serviceId,
+    clientId,
+    currentRequest: request,
+  });
+  const duration = `${service?.duration ?? 0} min`;
+  const onReschedule = () => {
+    router.push(`/requests/edit/${request._id}`);
+  };
+  const onCancel = () => {
+    deleteSelectedAction(request);
+    onClose();
+    router.push(`/requests/edit/${request._id}`);
+  };
+  return (
+    <Box>
+      <ViewField.Label>{statusMap?.[request?.status]}</ViewField.Label>
+      <ViewField>
+        <ViewField.Label>Cliente:</ViewField.Label>
+        <ViewField.Description>{client?.name}</ViewField.Description>
+      </ViewField>
+      <ViewField>
+        <ViewField.Label>Serviço:</ViewField.Label>
+        <ViewField.Description>{service?.name}</ViewField.Description>
+      </ViewField>
+      <ViewField>
+        <ViewField.Label>Duração:</ViewField.Label>
+        <ViewField.Description>{duration}</ViewField.Description>
+      </ViewField>
+      <ViewField>
+        <ViewField.Label>Preço:</ViewField.Label>
+        <ViewField.PriceText>{service?.price}</ViewField.PriceText>
+      </ViewField>
+      <Box position="relative" float="right" mt={10}>
+        {[0, 1, 7].includes(request?.status) && (
+          <>
+            <Button onClick={onCancel} colorScheme="red">
+              Cancelar
+            </Button>
+            <Button colorScheme="primary" ml={6} onClick={onReschedule}>
+              Reagendar
+            </Button>
+          </>
+        )}
+      </Box>
+    </Box>
+  );
 };
--- src/shared/ui/molecules/DateDetails/DateDetails.tsx
+++ src/shared/ui/molecules/DateDetails/DateDetails.tsx
@@ -1,8 +1,3 @@
-import { DateDetails as DateDetailsChakra, DateDetailsProps } from "@chakra-ui/react";
-export const DateDetails = ({ children, ...rest }: DateDetailsProps) => {
-  return (
-    <DateDetailsChakra {...rest} data-testid="DateDetailsTestId">
-      {children}
-    </DateDetailsChakra>
-  );
+export const DateDetails = ({ children, ...rest }) => {
+  return <>{children}</>;
 };
