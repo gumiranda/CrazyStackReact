a9f70a536854a3320b659cc66e924bd09fd8ae5e
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-05-09 18:36:56 -0300

    feat: Add user payment check before rendering page
    Co-authored-by: [Author Name] <[Author Email]>

--- src/app/users/create/page.tsx
+++ src/app/users/create/page.tsx
@@ -4,11 +4,17 @@ import type { Metadata } from "next";
 import { getServices } from "@/entidades/service/service.api";
 import { parseCookies, getCookies } from "@/shared/libs/utils";
 import { getOwners } from "@/entidades/owner";
+import { getUser } from "@/shared/libs/utils/layoutPayAsync";
+import { redirect } from "next/navigation";
 
 export const revalidate = 3000;
 async function getData(pageNumber) {
   const allCookies = getCookies();
   if (!allCookies) return null;
+  const user = await getUser();
+  if (user?.daysToNextCharge && user?.daysToNextCharge < 0) {
+    return { userHaveToPay: true, data: {}, owner: {} } as any;
+  }
   const [data, owner] = await Promise.all([
     getServices(pageNumber, parseCookies(allCookies), {}),
     getOwners(pageNumber, parseCookies(allCookies), {}),
@@ -16,7 +22,12 @@ async function getData(pageNumber) {
   if (!data || !owner) {
     return null;
   }
-  return { data, owner };
+
+  return {
+    userHaveToPay: user?.daysToNextCharge && user?.daysToNextCharge < 0,
+    data,
+    owner,
+  };
 }
 export const metadata: Metadata = {
   title: `${config.systemName} | Criar Profissional`,
@@ -25,6 +36,9 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   const data = await getData(1);
+  if (data?.userHaveToPay) {
+    redirect("/payment/pix");
+  }
   if (!data) return null;
   return <UserCreatePage data={data?.data} owner={data?.owner} />;
 }
