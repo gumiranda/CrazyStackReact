6e1aac5b2b204415a0c0e53dd3c077df4428d015
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-04 17:33:13 -0300

    feat: Add MobileNavbar component for responsive navigation

---
+++ src/widgets/NavBar/MobileNavbar.tsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerProps,
+  Stack,
+} from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+
+export const MobileDrawer = (props: Omit<DrawerProps, "children">) => {
+  const { t } = useTranslation(["LANDING"]);
+  return (
+    <Drawer placement="left" {...props}>
+      <DrawerContent bgColor={"secondary.500"}>
+        <DrawerBody my="16">
+          <Stack spacing="6" align="stretch">
+            {["Components", "Pricing", "Marketplace", "Support"].map((item) => (
+              <Button
+                bgColor={"secondary.500"}
+                color="white"
+                _hover={{ bgColor: "secondary.600" }}
+                key={item}
+                size="lg"
+                colorScheme="gray"
+              >
+                {item}
+              </Button>
+            ))}
+            <Button
+              bgColor="primary.600"
+              color="white"
+              _hover={{ bgColor: "primary.700" }}
+            >
+              Sign Up
+            </Button>
+          </Stack>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+};
