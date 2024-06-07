32d17f9a167d9b67794ea42a592d008d3a23d3fa
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-07 10:15:27 -0300

    feat: Add SalesLandingPage component for sales landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,14 @@ import { getCookies } from "@/shared/libs/utils/cookies";
 import { redirect } from "next/navigation";
 import type { Metadata } from "next";
 import { config } from "@/application/config";
+import { SalesLandingPage } from "@/slices/landing-page/pages/SalesLandingPage";
 
 export const metadata: Metadata = {
   title: `${config.systemName} | Agendamentos`,
-  description: "Agende seu horário com os melhores barbeiros da cidade",
+  description:
+    "Conheça nossa plataforma de  agendamentos de serviços, cadastre já o seu negócio!",
 };
-async function getData() {
-  const allCookies = getCookies();
-  if (!allCookies) {
-    return null;
-  }
-  const cookies = parseCookies(allCookies);
-  if (!cookies) {
-    return null;
-  }
-  return cookies;
-}
 
-export default async function Page() {
-  const cookies = await getData();
-  if (cookies?.["belezixadmin.token"]) {
-    redirect("/home");
-  }
-  return <Login />;
+export default function Page() {
+  return <SalesLandingPage />;
 }
---
+++ src/slices/landing-page/pages/SalesLandingPage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Flex } from "@/shared/ui";
+
+export const SalesLandingPage = () => {
+  return (
+    <Flex direction={"column"} flex="1">
+      <></>
+    </Flex>
+  );
+};
