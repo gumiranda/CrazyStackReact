f27db037c966b675e43dd700bc200f716e9fc546
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-10 12:11:23 -0300

    feat: Add FAQSection component to landing page
    The commit adds a new component called FAQSection to the landing page. This component includes an accordion-style FAQ section with questions and answers. It enhances the user experience by providing a convenient way to display frequently asked questions and their corresponding answers on the landing page.

--- src/slices/landing-page/components/templates/LayoutWithStickyNavbar/Main.tsx
+++ src/slices/landing-page/components/templates/LayoutWithStickyNavbar/Main.tsx
@@ -1,10 +1,12 @@
 import { config } from "@/application/config";
 import { Box, Flex, Text, ChakraLink, Image } from "@/shared/ui";
 import { CookieFooter } from "../../molecules/CookiesPreferences";
+import { FAQSection } from "../../organisms/FAQSection";
 
 export const Main = (props: any) => {
   return (
     <Flex as="main" role="main" direction="column" flex="1" py="0" {...props}>
+      <FAQSection />
       <CookieFooter />
     </Flex>
   );
