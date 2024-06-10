2ba0b2bb815e2032d3603eee720b156f1a65e2bc
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-10 12:26:11 -0300

    feat: Add SectionHowItWorks component to Main layout in landing page

--- src/slices/landing-page/components/templates/LayoutWithStickyNavbar/Main.tsx
+++ src/slices/landing-page/components/templates/LayoutWithStickyNavbar/Main.tsx
@@ -2,10 +2,12 @@ import { config } from "@/application/config";
 import { Box, Flex, Text, ChakraLink, Image } from "@/shared/ui";
 import { CookieFooter } from "../../molecules/CookiesPreferences";
 import { FAQSection } from "../../organisms/FAQSection";
+import { SectionHowItWorks } from "../../organisms/SectionHowItWorks";
 
 export const Main = (props: any) => {
   return (
     <Flex as="main" role="main" direction="column" flex="1" py="0" {...props}>
+      <SectionHowItWorks />
       <FAQSection />
       <CookieFooter />
     </Flex>
