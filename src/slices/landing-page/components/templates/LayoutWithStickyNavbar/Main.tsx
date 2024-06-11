6c33ff0003b4aec872d49cefbceb905bfd986c95
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-11 14:30:48 -0300

    feat: Add HeroWithSignUp component to Main layout in landing page

--- src/slices/landing-page/components/templates/LayoutWithStickyNavbar/Main.tsx
+++ src/slices/landing-page/components/templates/LayoutWithStickyNavbar/Main.tsx
@@ -5,10 +5,12 @@ import { FAQSection } from "../../organisms/FAQSection";
 import { SectionHowItWorks } from "../../organisms/SectionHowItWorks";
 import { SectionFeatures } from "../../organisms/SectionFeatures";
 import { WithImageBackground } from "../../organisms/WithImageBackground";
+import { HeroWithSignUp } from "../../organisms/HeroWithSignUp";
 
 export const Main = (props: any) => {
   return (
     <Flex as="main" role="main" direction="column" flex="1" py="0" {...props}>
+      <HeroWithSignUp />
       <WithImageBackground />
       <SectionFeatures />
       <SectionHowItWorks />
