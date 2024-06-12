4a4030f5d1557a98d89fdd6e4db358c22f4f40ff
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-12 09:48:45 -0300

    feat: Refactor CreateItemActions component and add styling enhancements
    This commit refactors the CreateItemActions component and introduces styling enhancements. The refactoring includes adding the keyframes for fadeIn and pulse animations, optimizing imports, and improving code readability. Additionally, the component now has a rounded border, a box shadow, and a transition effect. These changes enhance the visual appeal and user experience of the CreateItemActions component.
    
    Co-authored-by: [Author Name] <[Author Email]>

--- src/shared/ui/molecules/CreateItemActions/CreateItemActions.tsx
+++ src/shared/ui/molecules/CreateItemActions/CreateItemActions.tsx
@@ -1,10 +1,34 @@
-import { Link, HStack } from "@chakra-ui/react";
+import { Link, HStack, keyframes } from "@chakra-ui/react";
 import { Flex, Button } from "@/shared/ui";
 import { useTranslation } from "react-i18next";
+
 interface CreateItemActionsProps {
   isLoadingSaveButton: boolean;
   cancelRoute: string;
 }
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+const pulse = keyframes`
+0% {
+  transform: scale(1);
+  box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.7);
+}
+50% {
+  transform: scale(1.05);
+  box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+}
+100% {
+  transform: scale(1);
+  box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+}
+`;
 
 export const CreateItemActions = ({
   isLoadingSaveButton,
@@ -13,10 +37,25 @@ export const CreateItemActions = ({
   const { t } = useTranslation(["PAGES"]);
 
   return (
-    <Flex mt="8" justify="flex-end">
+    <Flex
+      mt="8"
+      justify="flex-end"
+      p={4}
+      borderRadius="md"
+      boxShadow={"md"}
+      transition="all 0.3s"
+      animation={`${fadeIn} 0.5s ease-in-out`}
+    >
       <HStack spacing="4">
         <Link href={cancelRoute}>
-          <Button colorScheme={"whiteAlpha"}>
+          <Button
+            bgColor="gray.300"
+            border="1px solid"
+            borderColor="gray.300"
+            borderRadius="full"
+            boxShadow={"base"}
+            _hover={{ bg: "gray.200", transform: "scale(1.25)" }}
+          >
             {t("PAGES:MESSAGES.cancel", {
               defaultValue: "Cancelar",
             })}
@@ -25,8 +64,12 @@ export const CreateItemActions = ({
         <Button
           type="submit"
           isLoading={isLoadingSaveButton}
-          _hover={{ bgColor: "tertiary.500" }}
+          _hover={{ bgColor: "tertiary.300" }}
           bgColor="tertiary.500"
+          borderRadius="full"
+          boxShadow={"base"}
+          color="white"
+          animation={`${pulse} 1.5s infinite`}
         >
           {t("PAGES:MESSAGES.save", {
             defaultValue: "Salvar",
