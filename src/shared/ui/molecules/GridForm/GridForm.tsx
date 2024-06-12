f0fd0d2aad40d06ed27617f94bbcd9743a6dfb14
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-12 09:56:00 -0300

    feat: Update GridForm component to accept additional props
    This commit updates the GridForm component to accept additional props. The changes include adding a spread operator to the component's props, allowing for more flexibility and customization. By passing additional props to the SimpleGrid component, developers can further customize the layout and behavior of the GridForm. This enhancement improves the reusability and versatility of the GridForm component.
    
    Co-authored-by: [Author Name] <[Author Email]>

--- src/shared/ui/molecules/GridForm/GridForm.tsx
+++ src/shared/ui/molecules/GridForm/GridForm.tsx
@@ -2,11 +2,12 @@ import { SimpleGrid } from "@chakra-ui/react";
 import { ReactNode } from "react";
 interface GridFormProps {
   children: ReactNode;
+  [key: string]: any;
 }
 
-export const GridForm = ({ children }: GridFormProps) => {
+export const GridForm = ({ children, ...props }: GridFormProps) => {
   return (
-    <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+    <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%" {...props}>
       {children}
     </SimpleGrid>
   );
