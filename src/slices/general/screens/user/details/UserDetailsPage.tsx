d98b8695ebff81cb089323affe9cd9085d0ca064
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-27 15:42:58 -0300

    feat: Add ProfileCard component for user details page
    This commit adds the ProfileCard component to the user details page. The code change includes creating a new file, ProfileCard.tsx, and importing necessary components from the shared/ui module. This enhancement improves the visual appearance and organization of the user details page.
    
    Co-authored-by: [Author Name] <[Author Email]>

--- src/slices/general/screens/user/details/UserDetailsPage.tsx
+++ src/slices/general/screens/user/details/UserDetailsPage.tsx
@@ -5,6 +5,7 @@ import { UserProps } from "@/slices/general/entidades/user";
 import { deleteUserById } from "@/slices/general/entidades/user/user.api";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/shared/libs";
+import { ProfileCard } from "./ProfileCard";
 
 type UserDetailsProps = {
   data: UserProps;
@@ -18,19 +19,26 @@ export const UserDetailsPage = ({ data, id, canDelete = false }: UserDetailsProp
   return (
     <>
       <Box flex="1" borderRadius={8} bg="secondary.500" p="8">
-        <UserDetails {...props} />
-        {canDelete === true && (
-          <Button
-            colorScheme="red"
-            onClick={async () => {
-              logout?.();
-              await deleteUserById(id, null);
-              router.push("/login");
-            }}
-          >
-            Deletar conta
-          </Button>
-        )}
+        <ProfileCard
+          userData={data}
+          handleOnClickButton={() => router.push(`/users/edit/${id}`)}
+        >
+          {canDelete === true && (
+            <Button
+              colorScheme="red"
+              onClick={async () => {
+                logout?.();
+                await deleteUserById(id, null);
+                router.push("/login");
+              }}
+              mt="4"
+              width="full"
+              _focus={{ boxShadow: "outline", borderColor: "red.500" }}
+            >
+              Deletar conta
+            </Button>
+          )}
+        </ProfileCard>
       </Box>
     </>
   );
