aba95f7617981b0cf487dec11d5c2ce9cdd28e11
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-10-28 18:41:31 -0300

    Refactor SalesLandingPage component to remove unnecessary fragment wrapper

--- src/slices/landing-page/pages/SalesLandingPage.tsx
+++ src/slices/landing-page/pages/SalesLandingPage.tsx
@@ -5,12 +5,10 @@ import { LayoutWithStickyNavbar } from "../components/templates";
 
 export const SalesLandingPage = () => {
   return (
-    <>
-      <Flex direction={"column"} flex="1">
-        <LayoutWithStickyNavbar.Navbar />
-        <LayoutWithStickyNavbar.Main />
-        <LayoutWithStickyNavbar.Footer />
-      </Flex>
-    </>
+    <Flex direction={"column"} flex="1">
+      <LayoutWithStickyNavbar.Navbar />
+      <LayoutWithStickyNavbar.Main />
+      <LayoutWithStickyNavbar.Footer />
+    </Flex>
   );
 };
