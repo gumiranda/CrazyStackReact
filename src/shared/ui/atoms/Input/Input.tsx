50ec8aaafc1bdca13df2a5cb46b3088982ed13a5
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-10-28 17:13:08 -0300

    Refactor Input component to remove unused import and simplify props

--- src/shared/ui/atoms/Input/Input.tsx
+++ src/shared/ui/atoms/Input/Input.tsx
@@ -1,8 +1,6 @@
-import { Input as InputChakra, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { Input as InputChakra } from "@chakra-ui/react";
 import { memo } from "react";
-interface InputProps extends ChakraInputProps {
-  name: string;
-}
+
 const InputAtom = ({ name, ...rest }) => {
   return <InputChakra {...rest} data-testid="InputTestId" id={name} name={name} />;
 };
