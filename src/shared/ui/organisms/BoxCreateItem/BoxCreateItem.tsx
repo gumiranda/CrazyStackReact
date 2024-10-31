07433d40f202ddf2a6861e3a7a396ba712f7bcdf
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-10-31 15:29:18 -0300

    Refactor BoxCreateItem component to add ClientOnly wrapper

--- src/shared/ui/organisms/BoxCreateItem/BoxCreateItem.tsx
+++ src/shared/ui/organisms/BoxCreateItem/BoxCreateItem.tsx
@@ -1,6 +1,7 @@
 //@ts-nocheck
-import { Separator, Heading, VStack } from "@chakra-ui/react";
 import { CreateItemActions, Box } from "@/shared/ui";
+import { ClientOnly, Skeleton, Separator, Heading, VStack } from "@chakra-ui/react";
+
 interface BoxCreateItemProps {
   onSubmit: any;
   title: string;
@@ -16,23 +17,25 @@ export const BoxCreateItem = ({
   title = "Criar novo registro",
 }: BoxCreateItemProps) => {
   return (
-    <Box
-      as="form"
-      flex="1"
-      borderRadius={8}
-      bg="secondary.500"
-      p={["0", "2", "3", "4"]}
-      onSubmit={onSubmit}
-    >
-      <Heading size="lg" fontWeight={"normal"}>
-        {title}
-      </Heading>
-      <Separator my="6" borderColor="secondary.400" />
-      <VStack gap={["6", "8"]}>{children}</VStack>
-      <CreateItemActions
-        isLoadingSaveButton={isLoadingSaveButton}
-        cancelRoute={cancelRoute}
-      />
-    </Box>
+    <ClientOnly fallback={<Skeleton />}>
+      <Box
+        as="form"
+        flex="1"
+        borderRadius={8}
+        bg="secondary.500"
+        p={["0", "2", "3", "4"]}
+        onSubmit={onSubmit}
+      >
+        <Heading size="lg" fontWeight={"normal"}>
+          {title}
+        </Heading>
+        <Separator my="6" borderColor="secondary.400" />
+        <VStack gap={["6", "8"]}>{children}</VStack>
+        <CreateItemActions
+          isLoadingSaveButton={isLoadingSaveButton}
+          cancelRoute={cancelRoute}
+        />
+      </Box>
+    </ClientOnly>
   );
 };
