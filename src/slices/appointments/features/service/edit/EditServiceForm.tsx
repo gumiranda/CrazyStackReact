271c7073de5c29d8f00851d053fa7813ec45c7b9
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-03 06:55:41 -0300

    Refactor EditServiceForm to integrate ProfilePhotoCover component and streamline form structure

--- src/slices/appointments/features/service/edit/EditServiceForm.tsx
+++ src/slices/appointments/features/service/edit/EditServiceForm.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/shared/ui";
 import { GetCategorysResponse } from "@/slices/appointments/entidades/category";
 import { useTranslation } from "react-i18next";
+import { ProfilePhotoCover } from "../../owner/edit/ProfilePhotoCover";
 
 export interface EditServiceFormProps {
   service: ServiceProps;
@@ -37,147 +38,148 @@ export const EditServiceForm = ({ service, categoryList }: EditServiceFormProps)
     service,
     categoryList,
   });
+  // const handleCoverChange = async (photoUploaded) => {
+  //   if (service?._id) {
+  //     await updateService({ cover: photoUploaded, _id: service?._id });
+  //     return;
+  //   }
+  // };
+  // const handleProfileChange = async (photoUploaded) => {
+  //   if (service?._id) {
+  //     await updateService({ photo: photoUploaded, _id: service?._id });
+  //     return;
+  //   }
+  // };
   return (
-    <BoxCreateItem
-      onSubmit={handleSubmit(handleEditService)}
-      title={t("PAGES:HOME_PAGE.editDomain", {
-        defaultValue: "Editar serviço",
-        domain: t("PAGES:HOME_PAGE.service", {
-          defaultValue: "Serviço",
-        }),
-      })}
-      isLoadingSaveButton={formState.isSubmitting}
-      cancelRoute={"/services/1"}
-    >
-      <GenericDetailsItem
-        item={service}
-        fields={[
-          { id: "_id", label: "Id" },
-          {
-            id: "name",
-            label: t("PAGES:FIELDS.name", {
+    <>
+      {/* <ProfilePhotoCover
+        coverImage={service?.cover}
+        handleCoverChange={handleCoverChange}
+        profileImage={service?.profilephoto}
+        handleProfileChange={handleProfileChange}
+      /> */}
+      <BoxCreateItem
+        onSubmit={handleSubmit(handleEditService)}
+        title={t("PAGES:HOME_PAGE.editDomain", {
+          defaultValue: "Editar serviço",
+          domain: t("PAGES:HOME_PAGE.service", {
+            defaultValue: "Serviço",
+          }),
+        })}
+        isLoadingSaveButton={formState.isSubmitting}
+        cancelRoute={"/services/1"}
+      >
+        <GridForm>
+          <FormControl
+            label={t("PAGES:FIELDS.name", {
               defaultValue: "Nome",
-            }),
-          },
-          { id: "createdById", label: "Id do criador" },
-          {
-            id: "createdAt",
-            label: t("PAGES:FIELDS.createdAt", {
-              defaultValue: "Data de criação",
-            }),
-          },
-        ]}
-      />
-      <GridForm>
-        <FormControl
-          label={t("PAGES:FIELDS.name", {
-            defaultValue: "Nome",
-          })}
-          error={formState.errors.name}
-          {...register("name")}
-        />
-        <FormControl
-          label={t("PAGES:FIELDS.description", {
-            defaultValue: "Descrição",
-          })}
-          error={formState.errors.description}
-          {...register("description")}
-        />
-        <FormControl
-          label={t("PAGES:FIELDS.price", {
-            defaultValue: "Preço promocional",
-          })}
-          error={formState.errors.price}
-          {...register("price")}
-        />
-        <FormControl
-          label={t("PAGES:FIELDS.finalPrice", {
-            defaultValue: "Preço do serviço",
-          })}
-          error={formState.errors.finalPrice}
-          {...register("finalPrice")}
-        />
-        <FormControl
-          label={t("PAGES:FIELDS.durationOfService", {
-            defaultValue: "Duração do serviço (em min)",
-          })}
-          type={"number"}
-          error={formState.errors.duration}
-          {...register("duration")}
-        />
-        <FormControl
-          label={t("PAGES:FIELDS.productsQuantityNeeded", {
-            defaultValue: "Quantidade de produtos necessários",
-          })}
-          type={"number"}
-          error={formState.errors.productsQuantityNeeded}
-          {...register("productsQuantityNeeded")}
-        />
-        <FormControl
-          label={t("PAGES:FIELDS.comission", {
-            defaultValue: "Comissão",
-          })}
-          type={"number"}
-          error={formState.errors.comission}
-          {...register("comission")}
-        />
-        <Select
-          bg="secondary.600"
-          name="categoryList"
-          label={t("PAGES:HOME_PAGE.category", {
-            defaultValue: "Categoria",
-          })}
-          list={categorys}
-          value={categorySelected}
-          onChange={handleChangeCategorySelected}
-          keyValue="_id"
-          keyLabel="name"
-        ></Select>
-        <Checkbox
-          label={t("PAGES:FIELDS.active", {
-            defaultValue: "Ativo",
-          })}
-          colorPalette="green"
-          checked={active}
-          onChange={(e) => {
-            e.preventDefault();
-            setActive(e.target.checked);
-          }}
-        />
-        <Checkbox
-          colorPalette="green"
-          label={t("PAGES:FIELDS.havePromotionalPrice", {
-            defaultValue: "Possui preço promocional?",
-          })}
-          checked={havePromotionalPrice}
-          onChange={(e) => {
-            e.preventDefault();
-            setHavePromotionalPrice(e.target.checked);
-          }}
-        />
-        <Checkbox
-          colorPalette="green"
-          label={t("PAGES:FIELDS.hasFidelityGenerator", {
-            defaultValue: "Gera pontos de fidelidade?",
-          })}
-          checked={hasFidelityGenerator}
-          onChange={(e) => {
-            e.preventDefault();
-            setHasFidelityGenerator(e.target.checked);
-          }}
-        />
-        <Checkbox
-          colorPalette="green"
-          label={t("PAGES:FIELDS.canPayWithFidelityPoints", {
-            defaultValue: "Pontos de fidelidade podem ser usados na compra?",
-          })}
-          checked={canPayWithFidelityPoints}
-          onChange={(e) => {
-            e.preventDefault();
-            setCanPayWithFidelityPoints(e.target.checked);
-          }}
-        />
-      </GridForm>
-    </BoxCreateItem>
+            })}
+            error={formState.errors.name}
+            {...register("name")}
+          />
+          <FormControl
+            label={t("PAGES:FIELDS.description", {
+              defaultValue: "Descrição",
+            })}
+            error={formState.errors.description}
+            {...register("description")}
+          />
+          <FormControl
+            label={t("PAGES:FIELDS.price", {
+              defaultValue: "Preço promocional",
+            })}
+            error={formState.errors.price}
+            {...register("price")}
+          />
+          <FormControl
+            label={t("PAGES:FIELDS.finalPrice", {
+              defaultValue: "Preço do serviço",
+            })}
+            error={formState.errors.finalPrice}
+            {...register("finalPrice")}
+          />
+          <FormControl
+            label={t("PAGES:FIELDS.durationOfService", {
+              defaultValue: "Duração do serviço (em min)",
+            })}
+            type={"number"}
+            error={formState.errors.duration}
+            {...register("duration")}
+          />
+          <FormControl
+            label={t("PAGES:FIELDS.productsQuantityNeeded", {
+              defaultValue: "Quantidade de produtos necessários",
+            })}
+            type={"number"}
+            error={formState.errors.productsQuantityNeeded}
+            {...register("productsQuantityNeeded")}
+          />
+          <FormControl
+            label={t("PAGES:FIELDS.comission", {
+              defaultValue: "Comissão",
+            })}
+            type={"number"}
+            error={formState.errors.comission}
+            {...register("comission")}
+          />
+          <Select
+            bg="secondary.600"
+            name="categoryList"
+            label={t("PAGES:HOME_PAGE.category", {
+              defaultValue: "Categoria",
+            })}
+            list={categorys}
+            value={categorySelected}
+            onChange={handleChangeCategorySelected}
+            keyValue="_id"
+            keyLabel="name"
+          ></Select>
+          <Checkbox
+            label={t("PAGES:FIELDS.active", {
+              defaultValue: "Ativo",
+            })}
+            colorPalette="green"
+            checked={active}
+            onChange={(e) => {
+              e.preventDefault();
+              setActive(e.target.checked);
+            }}
+          />
+          <Checkbox
+            colorPalette="green"
+            label={t("PAGES:FIELDS.havePromotionalPrice", {
+              defaultValue: "Possui preço promocional?",
+            })}
+            checked={havePromotionalPrice}
+            onChange={(e) => {
+              e.preventDefault();
+              setHavePromotionalPrice(e.target.checked);
+            }}
+          />
+          <Checkbox
+            colorPalette="green"
+            label={t("PAGES:FIELDS.hasFidelityGenerator", {
+              defaultValue: "Gera pontos de fidelidade?",
+            })}
+            checked={hasFidelityGenerator}
+            onChange={(e) => {
+              e.preventDefault();
+              setHasFidelityGenerator(e.target.checked);
+            }}
+          />
+          <Checkbox
+            colorPalette="green"
+            label={t("PAGES:FIELDS.canPayWithFidelityPoints", {
+              defaultValue: "Pontos de fidelidade podem ser usados na compra?",
+            })}
+            checked={canPayWithFidelityPoints}
+            onChange={(e) => {
+              e.preventDefault();
+              setCanPayWithFidelityPoints(e.target.checked);
+            }}
+          />
+        </GridForm>
+      </BoxCreateItem>
+    </>
   );
 };
